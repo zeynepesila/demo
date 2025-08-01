620a1e11decf4327427eb19992c5c4e56dc348f9
zeynepesila <gurelzeynepesila@gmail.com>
2025-08-01 16:54:41 +0300

    01.08.2025 PART2

--- src/main/java/com/blog/demo/config/WebConfig.java
+++ src/main/java/com/blog/demo/config/WebConfig.java
@@ -6,22 +6,23 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-
 public class WebConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("http://localhost:5174")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
             }
         };
     }
 }
+
+
 
 
 
