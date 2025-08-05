24befbeea857da222948e4ee4db955f457d17b55
zeynepesila <gurelzeynepesila@gmail.com>
2025-08-05 13:12:20 +0300

    05.08.2025 part2

--- src/main/java/com/blog/demo/config/WebConfig.java
+++ src/main/java/com/blog/demo/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173") // Vue projesi burada çalışıyor
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -22,7 +22,8 @@ public class WebConfig {
         };
     }
 }
-}
+
+    
 
 
 
